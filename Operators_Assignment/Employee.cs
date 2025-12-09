8ba20f9320a8d6a4c122e5a40915594b8588fd6a
JagdeepSaran <saranjagdeep66@gmail.com>
2025-12-09 13:58:47 -0500

    Add new C# Part2 assignments

---
+++ Operators_Assignment/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OperatorsAssignment
+{
+    public class Employee
+    {
+        // Properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Overload == operator
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            // Compare Id values
+            return emp1.Id == emp2.Id;
+        }
+
+        // Overload != operator
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return emp1.Id != emp2.Id;
+        }
+
+        // Avoid warnings by overriding these methods
+        public override bool Equals(object obj)
+        {
+            Employee temp = obj as Employee;
+            return this.Id == temp.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+    }
+}
---
+++ Operators_Assignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating first employee
+            Employee emp1 = new Employee()
+            {
+                Id = 101,
+                FirstName = "John",
+                LastName = "Smith"
+            };
+
+            // Creating second employee
+            Employee emp2 = new Employee()
+            {
+                Id = 101, // same ID, so they should be equal
+                FirstName = "Michael",
+                LastName = "Brown"
+            };
+
+            // Compare using overloaded operators
+            bool resultEqual = emp1 == emp2;
+            bool resultNotEqual = emp1 != emp2;
+
+            Console.WriteLine("Are both employees equal? " + resultEqual);
+            Console.WriteLine("Are both employees different? " + resultNotEqual);
+
+            Console.ReadLine();
+        }
+    }
+}
